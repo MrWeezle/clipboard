7e98c57ee237a31994737a727fa73df34df1f092
Simon W <MrWeezle@users.noreply.github.com>
2015-05-19 15:36:41 +0200

    Update footer-tmpl.js

--- public/js/templates/clipboard/footer-tmpl.js
+++ public/js/templates/clipboard/footer-tmpl.js
@@ -1,6 +1,6 @@
 define( function () {
 	return [
-		'<p class="left">&copy; 2012 &ndash; Developed by <a href="http://www.emanuel-kluge.de/">Emanuel Kluge</a> &middot; Hosted by <a href="http://www.nodester.com/">Nodester</a></p>',
-		'<p class="right">Proudly powered by <a href="http://nodejs.org/">Node.js</a>, <a href="http://expressjs.com/">Express.js</a>, <a href="http://mongoosejs.com/">Mongoose.js</a>, <a href="http://documentcloud.github.com/backbone/">Backbone.js</a>, <a href="http://documentcloud.github.com/underscore/">Underscore.js</a>, <a href="http://requirejs.org/">Require.js</a> and <a href="http://jquery.com/">jQuery.js</a></p>'
+		'<p class="left">&copy; 2012 &ndash; Entwickelt <a href="http://www.emanuel-kluge.de/">Emanuel Kluge</a></p>',
+		'<p class="right">Powered by <a href="http://nodejs.org/">Node.js</a></p>',
 	].join('');
 });
