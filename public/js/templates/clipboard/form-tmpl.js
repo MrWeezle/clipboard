5cf3664107e67c67aded3276fb2b286b8a1cfb29
Simon W <MrWeezle@users.noreply.github.com>
2015-05-19 15:37:03 +0200

    Update form-tmpl.js

--- public/js/templates/clipboard/form-tmpl.js
+++ public/js/templates/clipboard/form-tmpl.js
@@ -1,16 +1,16 @@
 define(function () {
 	return [
-		'<input type="text" class="note_input" placeholder="Enter your note and hit return">',
+		'<input type="text" class="note_input" placeholder="Notiz eingeben und Enter drücken">',
 		'<button type="button" class="ir">Info</button>',
 		'<ul class="menu">',
 		'	<li>',
-		'		<span>Double-click note text for editing.</span>',
+		'		<span>Notiz doppelklicken zum Editieren.</span>',
 		'	</li>',
 		'	<li>',
 		'		<span>Do not post private notes! This service<br> doesn\'t require user authentication.</span>',
 		'	</li>',
 		'	<li>',
-		'		<a href="https://github.com/herschel666/clipboard">View source on GitHub</a>',
+		'		<a href="https://github.com/MrWeezle/clipboard">View forked source on GitHub</a>',
 		'	</li>',
 		'</ul>'
 	].join('');
